b0f2b02e7102727aae4ce3501fc5109b4f03b27e
aragakerubo <aragakerubo@gmail.com>
2023-06-01 10:30:12 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <limits.h>
+
+/**
+ * struct print - struct
+ * @c: char
+ * @f: function
+ */
+typedef struct print
+{
+	char *c;
+	int (*f)(va_list);
+} print_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_format(const char *format))(va_list);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+#endif /* MAIN_H */
+
