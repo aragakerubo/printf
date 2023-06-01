b6ea3e96bb063f866b00d335f3bfeca3ef064678
aragakerubo <aragakerubo@gmail.com>
2023-06-01 14:46:57 -0700

    4. Nothing in fine print is ever good news

--- _putchar.c
+++ _putchar.c
@@ -12,13 +12,18 @@ int _putchar(char c)
 	static char buffer[1024];
 	static int i;
 
-	if (c == -1 || i >= 1024)
+	if (c == -1)
+	{
+		i = 0;
+	}
+
+	if (c == -2 || i >= 1024)
 	{
 		write(1, buffer, i);
 		i = 0;
 	}
 
-	if (c != -1)
+	if (c != -1 && c != -2)
 	{
 		buffer[i++] = c;
 	}
