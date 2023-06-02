cd7c3ec006c39131a1dc8f5edd86a3b9686bbfc5
ndwalep <ndwalepatience@gmail.com>
2023-06-02 11:53:37 -0700

    printf

--- _putchar.c
+++ _putchar.c
@@ -9,14 +9,5 @@
  */
 int _putchar(char c)
 {
-	static char buffer[1024];
-	static int i;
-
-	if (c == -1 || i >= 1024)
-	{
-		write(1, buffer, i);
-		i = 0;
-	}
-
-	return (1);
+	return (write(1, &c, 1));
 }
